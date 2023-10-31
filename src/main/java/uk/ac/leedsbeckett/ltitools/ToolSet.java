ac23115ef42857c0d0fafd25718e52a866d7b6e6
Jon Maber <j.r.maber@leedsbeckett.ac.uk>
2023-10-31 11:56:29 +0000

    Starting to support (new, unreleased) autoregistration spec.

--- src/main/java/uk/ac/leedsbeckett/ltitools/ToolSet.java
+++ src/main/java/uk/ac/leedsbeckett/ltitools/ToolSet.java
@@ -21,7 +21,7 @@ import uk.ac.leedsbeckett.ltitoolset.annotations.ToolSetMapping;
  *
  * @author maber01
  */
-@ToolSetMapping( loginUrl="/login", launchUrl="/launch", jwksUrl="/jwks" )
+@ToolSetMapping( loginUrl="/login", launchUrl="/launch", jwksUrl="/jwks", autoRegUrl="/autoreg/*" )
 public class ToolSet
 {
 }
